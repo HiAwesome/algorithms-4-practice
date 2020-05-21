1ef9093bc08fb3e955df0b95d5f41094ccfeb5c7
moqi <moqimoqidea@gmail.com>
2020-05-21 21:52:47 +0800

    20200521

---
+++ python/c01/p001.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+def gcd(p: int, q: int):
+    if q == 0:
+        return p
+
+    return gcd(q, p % q)
+
+
+class GcdTest(unittest.TestCase):
+    @staticmethod
+    def test_gcd():
+        assert gcd(12, 8) == 4
+
+
+if __name__ == '__main__':
+    unittest.main()
