05745754f3314008e3e99902aae64684f90e922f
moqi <moqimoqidea@gmail.com>
2020-05-22 08:48:32 +0800

    20200522

---
+++ src/main/java/c01/P042Flips.java
@@ -0,0 +1,35 @@
+package c01;
+
+import edu.princeton.cs.algs4.Counter;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+/**
+ * @author moqi
+ * On 5/22/20 08:19
+ */
+
+public class P042Flips {
+
+    public static void main(String[] args) {
+        int T = Integer.parseInt(args[0]);
+        Counter heads = new Counter("heads");
+        Counter tails = new Counter("tails");
+
+        for (int t = 0; t < T; t++) {
+            if (StdRandom.bernoulli(0.5)) {
+                heads.increment();
+            } else {
+                tails.increment();
+            }
+        }
+
+        StdOut.println(heads);
+        StdOut.println(tails);
+
+        int d = heads.tally() - tails.tally();
+        StdOut.println("delta: " + Math.abs(d));
+
+    }
+
+}
